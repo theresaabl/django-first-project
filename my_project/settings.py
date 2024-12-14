03e617723cf5d45d8737bc53f218e9d99e5e7e2f
Theresa Abl <theresaabl@gmx.at>
2024-12-14 11:02:31 +0000

    Deploy to heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -26,10 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    os.environ.get('HOST')
+    os.environ.get('HOST'),
+    '.herokuapp.com'
     ]
 
 
